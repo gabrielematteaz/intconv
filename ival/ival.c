006948384a2a02f016b708fbf4a5986811703917
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-13 11:02:46 +0200

    new version

--- ival/ival.c
+++ ival/ival.c
@@ -1,89 +1,48 @@
 #include "../mttlib/mttopt/mttopt.h"
 #include "../mttlib/mttstr/mttstr.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	struct mttopt_opt_t optv[] = {
-		{ 'f', 0, MUST_HAVE_ARG, 0 },
-		{ 't', 0, MUST_HAVE_ARG, 0 },
-		{ 'u', IGNORE_COPIES, 0, 0 }
+		{ 'f', OPT_FLAGS_MUST_HAVE_ARG, 0 },
+		{ 't', OPT_FLAGS_MUST_HAVE_ARG, 0 },
+		{ 'u', OPT_FLAGS_HAS_NO_ARG, 0 },
 	};
 
-	int avoff = mttopt_extract_optv(argc, argv, sizeof(optv) / sizeof(*optv), optv);
-	struct mttstr_fmt_t optfmt = { '+', '-', ' ', 10, 0, 0 }, tofmt, fromfmt;
-	size_t maxival, ival;
-	char *fstr, **av, *arg;
+	char **av = argv + mttopt_extr_optv(argc, argv, 3, optv), **avc = argv + argc, fstr[64 + 1];
+	struct mttstr_fmt_t optfmt = { 10, '+', '-', ' ', FMT_FLAGS_LEFT_FILL }, fromfmt = { .plus = '+', '-', ' ', FMT_FLAGS_MCASE | FMT_FLAGS_LEFT_FILL }, tofmt = { .flags = FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 };
 
-	if (optv[1].status == FOUND)
+	fromfmt.base = mttstr_fstr_to_ival(optv[0].arg, NULL, optfmt);
+
+	if (fromfmt.base < 2 || fromfmt.base > 36) fromfmt.base = 10;
+
+	tofmt.base = mttstr_fstr_to_ival(optv[1].arg, NULL, optfmt);
+
+	if (tofmt.base < 2 || tofmt.base > 36)
 	{
-		tofmt.base = mttstr_fstr_to_ival(optv[1].arg, NULL, optfmt);
+		tofmt.base = 16;
 
-		if (tofmt.base < 2 || tofmt.base > 36) goto default_tofmt;
+		goto uns;
+	}
 
-		if (tofmt.base == 10 && optv[2].status == NOT_FOUND)
-		{
-			tofmt.minus = '-';
-			tofmt.flags = NULL_TERM;
-			maxival = (size_t)1 << (sizeof(size_t) * CHAR_BIT - 1);
-		}
-		else goto not_signed_decimal_tofmt;
+	if (tofmt.base == 10 && optv[2].found == 0)
+	{
+		tofmt.plus = '+';
+		tofmt.minus = '-';
 	}
 	else
 	{
-	default_tofmt:
-		tofmt.base = 16;
-	not_signed_decimal_tofmt:
+	uns:
+		tofmt.plus = 0;
 		tofmt.minus = 0;
-		tofmt.flags = NULL_TERM;
-		maxival = -1;
 	}
 
-	tofmt.plus = 0;
-	tofmt.fillmode = 0;
-	tofmt.width = 0;
-	tofmt.width = mttstr_ival_to_fstr(NULL, maxival, tofmt);
-	fstr = malloc(tofmt.width + 1);
-
-	if (fstr != NULL)
+	while (av < avc)
 	{
-		if (optv[0].status == FOUND)
-		{
-			fromfmt.base = mttstr_fstr_to_ival(optv[0].arg, NULL, optfmt);
-
-			if (fromfmt.base < 2 || fromfmt.base > 36) goto default_fromfmt;
-
-			if (fromfmt.base == 10)	goto signed_decimal_fromfmt;
-
-			fromfmt.plus = 0;
-			fromfmt.minus = 0;
-		}
-		else
-		{
-		default_fromfmt:
-			fromfmt.base = 10;
-		signed_decimal_fromfmt:
-			fromfmt.plus = '+';
-			fromfmt.minus = '-';
-			fromfmt.flags = MCASE;
-		}
-
-		fromfmt.fill = ' ';
-		fromfmt.fillmode = 0;
-		av = argv + avoff;
-		arg = *av;
-
-		while (arg != NULL)
-		{
-			av++;
-			ival = mttstr_fstr_to_ival(arg, NULL, fromfmt);
-			mttstr_ival_to_fstr(fstr, ival, tofmt);
-			puts(fstr);
-			arg = *av;
-		}
-
-		free(fstr);
+		mttstr_ival_to_fstr(fstr, mttstr_fstr_to_ival(*av, NULL, fromfmt), tofmt);
+		av++;
+		puts(fstr);
 	}
 
 	return 0;
--- mttlib/mttstr/mttstr.h
+++ mttlib/mttstr/mttstr.h
@@ -2,39 +2,34 @@
 #define MTTSTR_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include <limits.h>
 
 void *mttstr_mem_rev(void *mem, size_t n);
 
-enum mttstr_fmt_fillmode_t
-{
-	LEFT,
-	INTERNAL,
-	RIGHT
-};
-
-enum mttstr_fmt_flags_t
-{
-	UCASE = 0,
-	LCASE = 1,
-	MCASE = 2,
-	DO_NOT_NULL_TERM = 0,
-	NULL_TERM = 4
-};
-
 struct mttstr_fmt_t
 {
-	char plus, minus, fill;
-	int base;
-	enum mttstr_fmt_fillmode_t fillmode;
-	enum mttstr_fmt_flags_t flags;
-	size_t width;
+	uint8_t base;
+	uint8_t plus, minus, fill;
+	uint8_t flags;
+	uint8_t width;
 };
+
+#define FMT_FLAGS_UCASE 0
+#define FMT_FLAGS_LCASE 1
+#define FMT_FLAGS_MCASE 2
+#define FMT_FLAGS_LEFT_FILL 0
+#define FMT_FLAGS_INT_FILL 4
+#define FMT_FLAGS_RIGHT_FILL 8
+#define FMT_FLAGS_DO_NOT_NULL_TERM 0
+#define FMT_FLAGS_NULL_TERM 16
+#define FMT_DEF_DEC { 10, '+', '-', ' ', FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
+#define FMT_DEF_HEX { 16, 0, 0, ' ', FMT_FLAGS_MCASE | FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
 
 #define IS_VAL_NEG(val) (val & (size_t)1 << (sizeof(val) * CHAR_BIT - 1))
 
 size_t mttstr_ival_to_fstr(char *fstr, size_t ival, struct mttstr_fmt_t fmt);
 
-size_t mttstr_fstr_to_ival(char *fstr, char **last, struct mttstr_fmt_t fmt);
+size_t mttstr_fstr_to_ival(const char *fstr, const char **last, struct mttstr_fmt_t fmt);
 
 #endif

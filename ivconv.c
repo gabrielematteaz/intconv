8afbb39320e9d789a9a2abe45e794a0cda78eb35
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-10-02 14:06:26 +0200

    new version

--- ivconv.c
+++ ivconv.c
@@ -1,4 +1,4 @@
-#include "mtt\cmd_line.h"
+#include "mtt\cmdline.h"
 #include "mtt\fmt.h"
 #include <stdio.h>
 
@@ -12,25 +12,19 @@ int main(int argc, char *argv[])
 	};
 
 	char **av = argv + mtt_extr_optv(argc, argv, sizeof(optv) / sizeof(*optv), optv), **argvc = argv + argc, fstr[128];
-	struct mtt_ivalfmt_t optfmt = { '+', '-', ' ', 10, FMT_FILLING_LEFT, 0 }, fromfmt = { '+', '-', ' ', 10, FMT_FILLING_LEFT | IVALFMT_LTR_CASE_UNK, 0 }, tofmt = { 0, 0, 0, 16, FMT_FILLING_LEFT | IVALFMT_LTR_CASE_UPPER, 0 };
+	struct mtt_ivalfmt_t fromfmt = { '+', '-', ' ', 10, FMT_FILLING_LEFT | VALFMT_LTR_CASE_UNK, 0 }, optfmt = { '+', 0, ' ', 10, FMT_FILLING_LEFT, 0 }, tofmt = { 0, 0, 0, 16, VALFMT_LTR_CASE_UPPER, 0 };
 
-	if (optv[0].arg)
-	{
-		fromfmt.base = mtt_fstr_to_ival(optv[0].arg, NULL, optfmt);
+	fromfmt.base = mtt_fstr_to_ival(optv[0].arg, NULL, optfmt);
 
-		if (fromfmt.base < 2 || fromfmt.base > 36) fromfmt.base = 10;
-	}
+	if (fromfmt.base < 2 || fromfmt.base > 36) fromfmt.base = 10;
 
 	if (optv[1].arg) tofmt.minus = '-';
 
-	if (optv[2].arg)
-	{
-		tofmt.base = mtt_fstr_to_ival(optv[2].arg, NULL, optfmt);
+	tofmt.base = mtt_fstr_to_ival(optv[2].arg, NULL, optfmt);
 
-		if (tofmt.base < 2 || tofmt.base > 36) tofmt.base = 16;
-	}
+	if (tofmt.base < 2 || tofmt.base > 36) tofmt.base = 16;
 
-	if (optv[3].arg) tofmt.fs |= IVALFMT_LTR_CASE_LOWER;
+	if (optv[3].arg) tofmt.fs |= VALFMT_LTR_CASE_LOWER;
 
 	while (av < argvc)
 	{
